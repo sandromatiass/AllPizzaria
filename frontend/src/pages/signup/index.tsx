604dc4a8722839702ea8b2d238087dd4366c12fe
sandromatiass <devsandromatias83@gmail.com>
2024-06-18 02:30:18 -0300

    correction

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -10,7 +10,13 @@ import { Input } from '@/components/ui/Input';
 import { Button } from '@/components/ui/Button';
 
 
+
+import { loginSchema } from '@/schemas/validationSchema';
+import { Form, Formik } from 'formik';
+
 export default function SignUp() {
+
+
   return (
     <>
     <Head>
@@ -19,32 +25,44 @@ export default function SignUp() {
     <div className={styles.containerCenter}>
       <Image src={imageLogo} alt="Logo AllPizzaria" priority/>
         <div className={styles.login}>
+        
         <h1>Criando sua conta</h1>
 
-        <form>
-        <Input 
-            placeholder='Digite seu Nome'
-            type='text'
-            autoComplete="off"
-          />
-          <Input 
-            placeholder='Digite seu email'
-            type='text'
-            autoComplete="off"
-          />
-          <Input 
-            placeholder='Digite sua senha'
-            type='password'
-            autoComplete="off"
-          />
+        <Formik  initialValues={{email: '', password: ''}}
+          validationSchema={loginSchema}
+          onSubmit={(values, {setSubmitting}) => {
+            setSubmitting(false);
+          }}>
+          {({ isSubmitting}) =>( 
+            <Form>
+              <Input
+                name='fullname' 
+                placeholder='Digite seu Nome'
+                type='text'
+                autoComplete="off"
+              />
+              <Input
+                name='email' 
+                placeholder='Digite seu email'
+                type='text'
+                autoComplete="off"
+              />
+              <Input
+                name='password' 
+                placeholder='Digite sua senha'
+                type='password'
+                autoComplete="off"
+              />
 
-          <Button
-            type="submit"
-            loading={false}
-          >
-            Cadastrar
-          </Button>
-        </form>
+              <Button
+                type="submit"
+                loading={false}
+              >
+                Cadastrar
+              </Button>
+            </Form>
+          )}  
+        </Formik>
         <Link href="/" className={styles.text}>
           Já possui uma conta.
         </Link>    
