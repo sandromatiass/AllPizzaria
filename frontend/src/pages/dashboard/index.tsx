848ff245488daaf609678cc19511d9b4625185aa
sandromatiass <devsandromatias83@gmail.com>
2024-06-24 21:12:17 -0300

    cofiguration initial

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,11 +1,37 @@
 import { canSSRAuth } from '@/utils/canSSRAuth';
 
 import Head from 'next/head';
+import styles from './styles.module.scss';
 
 import { Header } from '@/components/Header';
 
+import { FiRefreshCcw } from 'react-icons/fi';
 
-export default function DashBoard() {
+import { setupAPIClient } from '@/services/api';
+
+import { useState } from 'react';
+
+
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface DashboardProps{
+  orders: OrderProps[];
+}
+
+export default function DashBoard({orders}: DashboardProps) {
+
+  const [ orderList, setOrderList ] = useState(orders || []);
+
+  function handleOpenModalView(id: string){
+    alert('modal' + id)
+  }
+
   return (
    <>
     <Head>
@@ -13,6 +39,25 @@ export default function DashBoard() {
     </Head>
     <div>
       <Header/>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw size={25} color='#2C9A22' />
+          </button>
+        </div>
+          <article className={styles.listOreders}>
+          {orderList.map(item => (
+            <section key={item.id} className={styles.orderItem}>
+              <button onClick={ () => handleOpenModalView(item.id)}>
+                <div className={styles.tag}></div>
+                <span>Mesa {item.table}</span>
+              </button>
+            </section>
+          ))}
+          </article>
+        
+      </main>
     </div>
    </>
   )
@@ -20,7 +65,13 @@ export default function DashBoard() {
 
 export const getServerSideProps = canSSRAuth(async (ctx) =>{
 
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get('/orders');
+
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
